1c59957c1246cfbf6a038bd8e53c67ed947eafb8
TomiNumber <szabotamasjanos2@gmail.com>
2026-06-25 08:31:21 +0200

    Add DTOs and organize code

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param, Put } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { StockService } from './stock.service';
 import { SymbolParamDto } from './dto/finnhub-symbol.dto';
+import { StockPriceResponseDto } from './dto/stock-price.response.dto';
+import { PeriodicCheckResponseDto } from './dto/periodic-check.response.dto';
 
 @ApiTags('stock')
 @Controller('stock')
@@ -13,13 +15,17 @@ export class StockController {
   @ApiOperation({
     summary: 'Retrieve the current price, last updated time and moving average',
   })
-  getStock(@Param() { symbol }: SymbolParamDto) {
+  @ApiOkResponse({ type: StockPriceResponseDto })
+  getStock(@Param() { symbol }: SymbolParamDto): Promise<StockPriceResponseDto> {
     return this.stockService.getStock(symbol);
   }
 
   @Put('/:symbol')
   @ApiOperation({ summary: 'Start the periodic checks for a given symbol' })
-  startPeriodicCheck(@Param() { symbol }: SymbolParamDto) {
+  @ApiOkResponse({ type: PeriodicCheckResponseDto })
+  startPeriodicCheck(
+    @Param() { symbol }: SymbolParamDto,
+  ): Promise<PeriodicCheckResponseDto> {
     return this.stockService.startPeriodicCheck(symbol);
   }
 }
